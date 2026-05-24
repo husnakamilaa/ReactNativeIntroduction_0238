3dfa8f42f961bcb61a009180a569ecd86955caaf
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-05-24 13:25:33 +0700

    feat: menambahkan contoh tsx

---
+++ src/app/contoh.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const contoh = () => {
+  return (
+    <View>
+      <Text>contoh</Text>
+    </View>
+  )
+}
+
+export default contoh;
