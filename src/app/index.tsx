b835274d85a57b632286d6543ebff7a956222d45
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-05-24 15:19:58 +0700

    feat: deklarasi variable username dan password untuk login dan function handle submit, dan melakukan navigasi ke halaman dashboard dengan membawa variable username

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,36 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Link, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, TextInput, View, Alert, KeyboardAvoidingView, Platform, SafeAreaView, TouchableOpacity } from "react-native";
+import { Navbar } from "../components/navbar";
+import { COLORS, globalStyles } from "../style";
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <Link href="/contoh">Halaman Contoh</Link>
-      <Link href="/div">Halaman Form</Link>
-      <Text>Ini Form Nama </Text>
-      <TextInput
-        placeholder="Masukkan nama anda"
-        style={{ height: 40, borderColor: "gray", borderWidth: 1, width: 200 }}
-      />
-    </View>
-  );
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = () => {
+    if (!username || !password) {
+      Alert.alert("Error", "Semua kolom harus diisi!");
+      return;
+    }
+    router.replace({
+      pathname: "/dashboard",
+      params: { username },
+    });
+  };
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Hello</Text>
+  //     <Link href="/contoh">Halaman Contoh</Link>
+  //     <Link href="/div">Halaman Form</Link>
+  //     <Text>Ini Form Nama </Text>
+  //     <TextInput
+  //       placeholder="Masukkan nama anda"
+  //       style={{ height: 40, borderColor: "gray", borderWidth: 1, width: 200 }}
+  //     />
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
